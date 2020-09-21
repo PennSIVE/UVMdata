92d92a7caba022562b5a900c80267d4f745d7f6a
Tim <tim.terf@gmail.com>
2020-09-21 04:59:48 -0400

    weighted cvs

--- cvs/run_weighted.sh
+++ cvs/run_weighted.sh
@@ -6,13 +6,13 @@ do
     qsub -b y -cwd -l h_vmem=512G -pe threaded 1-20 -o ~/sge/\$JOB_ID -e ~/sge/\$JOB_ID \
     SINGULARITYENV_NSLOTS=\$NSLOTS singularity run --cleanenv \
     -B ~/repos/UVMdata/cvs:/code:ro \
-    -B ~/dropbox/UVMdata/${subj}/flair.nii.gz:/flair.nii.gz:ro \
-    -B ~/dropbox/UVMdata/${subj}/mprage.nii.gz:/t1.nii.gz:ro \
-    -B ~/dropbox/UVMdata/${subj}/T2star.nii.gz:/epi.nii.gz:ro \
+    -B ~/dropbox/jordan/${subj}/candidate.lesions.nii.gz:/lesmap.nii.gz:ro \
+    -B ~/dropbox/jordan/${subj}/frangi.nii.gz:/veinmap.nii.gz:ro \
+    -B ~/dropbox/jordan/${subj}/epi_brain.nii.gz:/epi.nii.gz:ro \
     -B ~/dropbox/jordan/${subj}:/out \
     --pwd /code \
     /cbica/home/robertft/singularity_images/neuror_4.0.sif \
-    Rscript run.R
+    Rscript run_weighted.R
 done
 
 
@@ -22,12 +22,12 @@ do
     qsub -b y -cwd -l h_vmem=512G -pe threaded 1-20 -o ~/sge/\$JOB_ID -e ~/sge/\$JOB_ID \
     SINGULARITYENV_NSLOTS=\$NSLOTS singularity run --cleanenv \
     -B ~/repos/UVMdata/cvs:/code:ro \
-    -B ~/bids/datasets/2020-06-07/Nifti_all_combined/${subj}/ses-001/anat/${subj}_ses-001_run-001_T2w.nii.gz:/flair.nii.gz:ro \
-    -B ~/bids/datasets/2020-06-07/Nifti_all_combined/${subj}/ses-001/anat/${subj}_ses-001_run-001_T1w.nii.gz:/t1.nii.gz:ro \
-    -B ~/bids/datasets/2020-06-07/Nifti_all_combined/${subj}/ses-001/anat/${subj}_ses-001_run-001_T2star1.nii.gz:/epi.nii.gz:ro \
+    -B ~/dropbox/jordan_new/${subj}/candidate.lesions.nii.gz:/lesmap.nii.gz:ro \
+    -B ~/dropbox/jordan_new/${subj}/frangi.nii.gz:/veinmap.nii.gz:ro \
+    -B ~/dropbox/jordan_new/${subj}/epi_brain.nii.gz:/epi.nii.gz:ro \
     -B ~/dropbox/jordan_new/${subj}:/out \
     --pwd /code \
     /cbica/home/robertft/singularity_images/neuror_4.0.sif \
-    Rscript run.R
+    Rscript run_weighted.R
 done
 
